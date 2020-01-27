262717d80049c25b7d956b01914ee850618ebd62
Kimbed <Kimbed@ONS24158>
2020-01-27 16:51:49 +0000

    LU-5185 powershell script for stackdriver install

---
+++ Stackdriver install.ps1
@@ -0,0 +1,3 @@
+﻿cd $env:UserProfile;
+(New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/windows/StackdriverLogging-v1-10.exe", "C:\dev\StackdriverLogging-v1-10.exe")
+. C:\dev\StackdriverLogging-v1-10.exe /S
