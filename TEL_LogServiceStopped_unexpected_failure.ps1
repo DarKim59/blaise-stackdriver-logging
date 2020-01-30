c6673f80a0fc32ac4e9b2bf3b7e2ab23596ed4ba
Kimbed <Kimbed@ONS24158>
2020-01-30 09:00:27 +0000

    Now writing to a 'temp' file and copying to the file being used by Stackdriver.

--- TEL_LogServiceStopped_unexpected_failure.ps1
+++ TEL_LogServiceStopped_unexpected_failure.ps1
@@ -8,7 +8,9 @@ param(
 function OutPutToServiceFailedFile($ServiceName)
 {
     [string]$msg = "$((Get-Date).ToString()) -- Service: " + $ServiceName + " has an unexpected failure"
-     Add-Content C:\BlaiseServices\TEL_BlaiseServices_Failure.log $msg
+    Add-Content C:\BlaiseServices\TEL_BlaiseServices_Failure_temp.log $msg
+
+    Copy-Item C:\BlaiseServices\TEL_BlaiseServices_Failure_temp.log C:\BlaiseServices\TEL_BlaiseServices_Failure.log
 }
 
 OutPutToServiceFailedFile($ServiceName)
