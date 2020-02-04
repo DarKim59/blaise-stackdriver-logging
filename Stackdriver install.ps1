5f7717ac3c8e241cb5ff9ce9986c3d379c0ee8ec
DarKim59 <Darren.kimber@sky.com>
2020-02-04 08:16:04 +0000

    Update Stackdriver install.ps1
    update to capture an exit code of the .exe that was invoked

--- Stackdriver install.ps1
+++ Stackdriver install.ps1
@@ -1,3 +1,11 @@
 ﻿cd $env:UserProfile;
 (New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/windows/StackdriverLogging-v1-10.exe", "C:\dev\StackdriverLogging-v1-10.exe")
 . C:\dev\StackdriverLogging-v1-10.exe /S
+if($LASTEXITCODE -eq 0)
+{
+    Write-Host "The last PS command executed successfully"
+} 
+else 
+{
+    Write-Host "The last PS command failed"
+}
