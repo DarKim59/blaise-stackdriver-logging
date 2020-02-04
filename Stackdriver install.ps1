ce209fb8d2ca0b5a148d742c6c194c0b7d871e62
DarKim59 <Darren.kimber@sky.com>
2020-02-04 16:11:53 +0000

    Update Stackdriver install.ps1

--- Stackdriver install.ps1
+++ Stackdriver install.ps1
@@ -1,16 +1,17 @@
 "Starting Download of Stackdriver exe"
-cd $env:UserProfile;
-(New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/windows/StackdriverLogging-v1-10.exe", "C:\dev\StackdriverLogging-v1-10.exe")
+#cd $env:UserProfile;
+cd C:\BlaiseServices\Stackdriver\
+(New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/windows/StackdriverLogging-v1-10.exe", "C:\BlaiseServices\Stackdriver\StackdriverLogging-v1-10.exe")
+& 'C:\BlaiseServices\Stackdriver\StackdriverLogging-v1-10.exe' /S /D="C:\BlaiseServices\Stackdriver\LoggingAgent\"
 
-"Download complete - Now running exe"
-
-& 'C:\dev\StackdriverLogging-v1-10.exe' /S /D="C:\BlaiseServices\Stackdriver\LoggingAgent\"
-
-if($LASTEXITCODE -eq 0)
+function WaitUntilServices($searchString, $status)
 {
-    Write-Host "The last PS command executed successfully"
-} 
-else 
-{
-    Write-Host "The last PS command failed"
+    # Get all services where DisplayName matches $searchString and loop through each of them.
+    foreach($service in (Get-Service -DisplayName $searchString))
+    {
+        # Wait for the service to reach the $status or a maximum of 30 seconds
+        $service.WaitForStatus($status, '00:01:30')
+    }
 }
+
+WaitUntilServices "Stackdriver Logging" "Running"
