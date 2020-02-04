e91287d0ee54624ea174e7306b79440376ac72e6
DarKim59 <Darren.kimber@sky.com>
2020-02-04 09:48:15 +0000

    Update Stackdriver install.ps1
    Added status messages and Stackdriver installation directory location

--- Stackdriver install.ps1
+++ Stackdriver install.ps1
@@ -1,6 +1,11 @@
-﻿cd $env:UserProfile;
+"Starting Download of Stackdriver exe"
+cd $env:UserProfile;
 (New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/windows/StackdriverLogging-v1-10.exe", "C:\dev\StackdriverLogging-v1-10.exe")
-. C:\dev\StackdriverLogging-v1-10.exe /S
+
+"Download complete - Now running exe"
+
+& 'C:\dev\StackdriverLogging-v1-10.exe' /S /D="C:\BlaiseServices\Stackdriver\Logging\"
+
 if($LASTEXITCODE -eq 0)
 {
     Write-Host "The last PS command executed successfully"
