547fa670eb6a6de8071f1639808de2adc0abde32
DarKim59 <Darren.kimber@sky.com>
2020-02-05 09:15:22 +0000

    Update Stackdriver install.ps1

--- Stackdriver install.ps1
+++ Stackdriver install.ps1
@@ -1,1 +1,2 @@
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""C:\Blaiseservices\Stackdriver\Stackdriver_Install_main.ps1""' -Verb RunAs}";
+Start-Process 'C:\windows\system32\notepad.exe'
+# PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""C:\Blaiseservices\Stackdriver\Stackdriver_Install_main.ps1""' -Verb RunAs}";
